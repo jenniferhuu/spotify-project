e916f0d832d187e83c696c376bc757f09593c4f6
Danny Zhang <zhang.dny@gmail.com>
2026-05-27 10:04:53 -0400

    completed the routing and backend for forums

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import forumsRouter from "./routes/forums.js"
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,7 @@ const host = process.env.HOST || "127.0.0.1";
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/forums", forumsRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
