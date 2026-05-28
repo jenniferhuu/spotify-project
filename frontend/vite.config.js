9538bd16fde596f85c6f21f81ce7606c5822d360
Kaitlyn23254 <kaitlyn23254@gmail.com>
2026-05-28 09:12:33 -0400

    feat: switch localhost URLs to 127.0.0.1 for spotify formatting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/topSongs': 'http://127.0.0.1:5000',
-        },
-    },
 });
